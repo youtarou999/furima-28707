4110ec602bce16b7a3cc4e39dc82c9d50695d328
youtarou999 <youtarou5@gmail.com>
2020-09-06 14:09:01 +0900

    テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,19 @@ class User < ApplicationRecord
 
   validates :name,                  presence: true
   validates :email,                 presence: true
-  validates :password,              presence: true
-  validates :password_confirmation, presence: true
-  validates :family_name,           presence: true
-  validates :first_name,            presence: true
-  validates :family_name_kana,      presence: true
-  validates :first_name_kana,       presence: true
+  
+  validates :password,              presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英字と数字の両方を含めて設定してください'} 
+  validates :password_confirmation, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英字と数字の両方を含めて設定してください'} 
+
+
+  validates :family_name,           presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください'} 
+  validates :first_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください'} 
+
+  
+  validates :family_name_kana,      presence: true, format: {with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' } 
+  validates :first_name_kana,       presence: true, format: {with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' }
+  
+
   validates :birthday,              presence: true
 
   
