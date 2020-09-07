b5f8ba0ea12d34a210265ac24bd2402650ba1729
youtarou999 <youtarou5@gmail.com>
2020-09-07 16:20:54 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,8 +13,8 @@ class ItemsController < ApplicationController
   end
 
   private
-  def tweet_params
-    params.require(:item).permit(:name, :introduction, :price, :category_id, :product_condition_id, :postage_payer_id, :preparation_days_id, :user_id)
+  def item_params
+    params.require(:item).permit(:name, :image, :introduction, :price, :category_id, :product_condition_id, :postage_payer_id, :preparation_days_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :name,                 length: { maximum: 40 }                
     validates :introduction,         length: { maximum: 1000 }                
     validates :price               
-    validates :category_id
-    validates :product_condition_id
-    validates :postage_payer_id
-    validates :preparation_days_id
-    validates :user_id
+    validates :category_id,          foreign_key: true
+    validates :product_condition_id, foreign_key: true
+    validates :postage_payer_id,     foreign_key: true
+    validates :preparation_days_id,  foreign_key: true
+    validates :user_id,              foreign_key: true
   end
 end
 
