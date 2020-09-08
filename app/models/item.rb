9ad9be09466e0b087ca8af4d40cb4fcb5f614b24
youtarou999 <youtarou5@gmail.com>
2020-09-08 12:06:36 +0900

    Activehush設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,7 @@
 class Item < ApplicationRecord
+  
   belongs_to :user
   has_one_attached :image
 
-  with_options presence: true do
-    validates :name,                 length: { maximum: 40 }                
-    validates :introduction,         length: { maximum: 1000 }                
-    validates :price               
-    validates :category_id,          foreign_key: true
-    validates :product_condition_id, foreign_key: true
-    validates :postage_payer_id,     foreign_key: true
-    validates :preparation_days_id,  foreign_key: true
-    validates :user_id,              foreign_key: true
-  end
+  
 end
-
