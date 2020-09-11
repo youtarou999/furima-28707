6d28c57d497f0f3ad88d59d288c0ddabf84dc158
youtarou999 <youtarou5@gmail.com>
2020-09-11 11:32:28 +0900

    未ログインユーザーを転送

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :move_to_index, except: :index
+
 
   def index
   end
@@ -21,6 +22,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :price, :category_id, :prefecture_id,
                                  :product_condition_id, :postage_payer_id, :preparation_days_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
