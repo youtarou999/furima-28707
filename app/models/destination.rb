054dba5d0c67ec360b4511f789e1c2be0ea5f7c0
youtarou999 <youtarou5@gmail.com>
2020-09-15 08:30:54 +0900

    balidates設定

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,15 @@
 class Destination < ApplicationRecord
   belongs_to :orders
+
+  POSTAL_REGEX = /\A\d{7}\z/
+  PHONE_REGEX  = /\A\d{10,11}\z/
+
+  with_options presence: true do
+    validates :postal_code,    format: {with: POSTAL_REGEX, message: 'ハイフンなしで11桁の数字を入力してください'}                 
+    validates :prefecture_id                             
+    validates :city   
+    validates :house_number
+    validates :phone_number,   format: {with: PHONE_REGEX, message: 'ハイフンなしで10~11桁の数字を入力してください'}                  
+  end
 end
+
