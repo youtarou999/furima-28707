a063267945e0f6c1108ce989812391023b99e65b
youtarou999 <youtarou5@gmail.com>
2020-09-15 08:58:25 +0900

    コントローラー設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,22 @@ class OrdersController < ApplicationController
   def index
   end
 
+  def new
+    @order = Order.new
+  end
+
   def create
+    @order = Order.create(order_params)
+    if @order.save
+      redirect_to action: :index
+   else
+      render "new"
+   end
+  end
+
+  private
+
+  def orfer_params
+    params.require(:order).merge(user_id: current_user.id, item_id:current_item.id )
   end
 end
