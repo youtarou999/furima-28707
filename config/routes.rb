b0c52bf476c56d4ac058ffa3bd2894da9c1d214c
youtarou999 <youtarou5@gmail.com>
2020-09-15 16:09:42 +0900

    購入画面遷移後の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
-  resources :orders, only: [:index, :new, :create]
-  
-  
+  resources :items do
+    resources :orders, only: [:index, :new, :create]
+  end
 end
