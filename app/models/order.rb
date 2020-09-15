563e3c212f926176204b20b94ab8142e61348a94
youtarou999 <youtarou5@gmail.com>
2020-09-15 09:56:50 +0900

    都道府県のアクティブハッシュ設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,8 @@
 class Order < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   has_one :destination
   belongs_to :user
   belongs_to :item
+  belongs_to_active_hash :prefecture
 end
