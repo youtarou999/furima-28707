db49c28fb431e3933efd7d12617a5885fc33f903
youtarou999 <youtarou5@gmail.com>
2020-09-17 15:27:31 +0900

    before_action定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :set_order, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
-    @order_destination = OrderDestination.new()
   end
 
   def new
@@ -9,22 +10,38 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = OrderDestination.new(order_params)
-
-    # if @order.valid?
-    #   pay_item
-    #   @order.save
-    #   return redirect_to root_path
-    # else
-    #   render 'new'
-    # end
+    
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
 
   end
 
   private
 
   def order_params
-    params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id)
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token, :authenticity_token, :order_destination, :item_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
- 
+
+  def pay_item
+    Payjp.api_key = "sk_test_6018ccc339c56ee8142b22c7"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def set_order
+    @order = OrderDestination.new(order_params)
+  end
+
 end
